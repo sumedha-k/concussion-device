ca121a2ae05f5cc3443179ad159a5f6013e4be9b
JuliaJZhong <juliazhongg@gmail.com>
2021-05-14 22:32:51 -0700

    updated calendar page a teensy bit more

--- app/src/main/java/com/example/concussionapp/Calendar.kt
+++ app/src/main/java/com/example/concussionapp/Calendar.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.concussionapp.R
 import android.animation.ValueAnimator
+import android.graphics.Color
 import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -39,6 +40,8 @@ class Calendar : Fragment() {
     // This property is only valid between onCreateView and onDestroyView.
     private val binding get() = _binding!!
 
+    private var selectedDate: LocalDate? = null
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         _binding = FragmentCalendarBinding.inflate(inflater, container, false)
@@ -67,17 +70,84 @@ class Calendar : Fragment() {
         class DayViewContainer(view: View) : ViewContainer(view) {
             val textView = view.findViewById<TextView>(R.id.calendarDayText)
 
-            // With ViewBinding
-            // val textView = CalendarDayLayoutBinding.bind(view).calendarDayText
+            lateinit var day: CalendarDay
+
+            init {
+                view.setOnClickListener {
+                    // Check the day owner as we do not want to select in or out dates.
+                    if (day.owner == DayOwner.THIS_MONTH) {
+                        // Keep a reference to any previous selection
+                        // in case we overwrite it and need to reload it.
+                        val currentSelection = selectedDate
+                        if (currentSelection == day.date) {
+                            // If the user clicks the same date, clear selection.
+                            selectedDate = null
+                            // Reload this date so the dayBinder is called
+                            // and we can REMOVE the selection background.
+                            calendarView.notifyDateChanged(currentSelection)
+                        } else {
+                            selectedDate = day.date
+                            // Reload the newly selected date so the dayBinder is
+                            // called and we can ADD the selection background.
+                            calendarView.notifyDateChanged(day.date)
+                            if (currentSelection != null) {
+                                // We need to also reload the previously selected
+                                // date so we can REMOVE the selection background.
+                                calendarView.notifyDateChanged(currentSelection)
+                            }
+                        }
+                    }
+                }
+            }
         }
+
+        // With ViewBinding
+        // val textView = CalendarDayLayoutBinding.bind(view).calendarDayText
 
         calendarView.dayBinder = object : DayBinder<DayViewContainer> {
             // Called only when a new container is needed.
             override fun create(view: View) = DayViewContainer(view)
-
             // Called every time we need to reuse a container.
             override fun bind(container: DayViewContainer, day: CalendarDay) {
-                container.textView.text = day.date.dayOfMonth.toString()
+                // Set the calendar day for this container
+                container.day = day
+                // Set the day text
+                val textView = container.textView
+                textView.text = day.date.dayOfMonth.toString()
+                if (day.owner == DayOwner.THIS_MONTH) {
+                    textView.alpha = 1f // sets this month's dates to 100% opacity
+                    // Show the month dates. Remember that views are recycled!
+                    // textView.visibility = View.VISIBLE
+                    if (day.date == selectedDate) {
+                        // If this is the selected date, show a round background and change the text color.
+                        // textView.setTextColor(Color.rgb(0x65,0x68,0xb8)) // same color as dayView background, figure out how to do this by referencing the color variable i made in colors.xml
+                        textView.setTextColor(Color.rgb(0x37, 0x00, 0xb3)) // same color as header background
+                        // textView.setTextColor(Color.BLACK)
+                        textView.setBackgroundResource(R.drawable.selection_background)
+                    } else {
+                        // If this is NOT the selected date, remove the background and reset the text color.
+                        textView.setTextColor(Color.rgb(0x37, 0x00, 0xb3))
+                        textView.background = null
+                    }
+                } else {
+                    // inDates and outDates appear, but grayed-out
+                    textView.setTextColor(Color.rgb(0x37, 0x00, 0xb3)) // sets in and outDate color
+                    textView.alpha = 0.2f // sets in and outDates to 50% opacity of above color. if you don't specify color in line above, automatically does gray
+                    // Hide in and out dates
+                    // textView.visibility = View.INVISIBLE
+                }
+            }
+        }
+
+        class MonthViewContainer(view: View) : ViewContainer(view) {
+            val textView = view.findViewById<TextView>(R.id.headerTextView)
+        }
+
+        calendarView.monthHeaderBinder = object : MonthHeaderFooterBinder<MonthViewContainer> {
+            override fun create(view: View) = MonthViewContainer(view)
+            override fun bind(container: MonthViewContainer, month: CalendarMonth) {
+                container.textView.text = "${month.yearMonth.month.name.toLowerCase().capitalize()} ${month.year}"
+                container.textView.setTextColor(Color.WHITE)
             }
         }
     }
